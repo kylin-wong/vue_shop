55f819b20ba2fb6e8072ea9d2c4dd2e60403af52
kylin-wong <375506637@qq.com>
2019-12-19 15:21:48 +0800

    登录页面样式

---
+++ src/components/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="login_container">
+      <div class="login_box">
+          <!-- 头像区域 -->
+          <div class="avatar-box">
+              <img src="../assets/logo.png" alt="">
+          </div>
+          <!-- 登录表单区域 -->
+          <el-form :model="loginForm" label-width="0px" class="login_form">
+              <!-- 用户名表单 -->
+            <el-form-item>
+                <el-input prefix-icon="iconfont icon-user"></el-input>
+            </el-form-item>
+            <!-- 密码表单 -->
+            <el-form-item>
+                <el-input prefix-icon="iconfont icon-3702mima
+"></el-input>
+            </el-form-item>
+            <el-form-item class="btns">
+                <el-button type="primary">登录</el-button>
+                <el-button type="info">重置</el-button>
+            </el-form-item>
+          </el-form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box{
+    position:absolute;
+    top: 50%;
+    left: 50%;
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    transform: translate(-50%,-50%)
+}
+.avatar-box{
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    box-shadow: 0 0 10px #ddd;
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    padding: 10px;
+    border: 1px solid #eee;
+    background-color: #fff;
+    img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+.login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.btns{
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
