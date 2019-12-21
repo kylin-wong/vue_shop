04079fef852cbfac7327f17968a5c37a85f4310e
kylin-wong <375506637@qq.com>
2019-12-21 20:50:57 +0800

    自定义邮箱手机校验规则

--- src/main.js
+++ src/main.js
@@ -7,10 +7,20 @@ import './plugins/element.js'
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+import Mbx from './components/Mbx/Mbx.vue'
 
 // 配置axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  // 将服务器发出的token放入 请求头中的Authorization字段 第一次登录期间 服务器没有颁发token令牌
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 面包屑导航
+Vue.component('mbx', Mbx)
 // 每一个vue的组件 都可以通过this 直接访问到$http 发起ajax请求
 // this.$http 就是axios请求
 Vue.prototype.$http = axios
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,12 +4,48 @@ import {
   Form,
   FormItem,
   Input,
-  Message
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 // message挂载到vue的原型上
 Vue.prototype.$message = Message
