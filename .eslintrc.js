26d63aeb694e76526af12bd5624e6c8d879ef864
kylin-wong <375506637@qq.com>
2019-12-24 11:28:12 +0800

    完成了权限功能的开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,12 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'space-before-function-paren': 0,
     'no-useless-return': 0,
-    'indent': 0
+    'indent': 0,
+    'vue/no-unused-vars': 0,
+    'no-undef': 0,
+    'no-trailing-spaces': 0,
+    'vue/no-parsing-error': 0,
+    'no-unused-vars': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
